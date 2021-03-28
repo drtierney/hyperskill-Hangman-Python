70ed4e64769fac5613bf0b59d8465a73ba2f054f
drtierney <d.r.tierney@hotmail.co.uk>
2021-03-28 16:38:14 +0100

    Stage #7 completed
    Stage 7/8: Error!

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,4 +1,5 @@
 import random
+from string import ascii_lowercase
 
 
 def play_game(words, lives):
@@ -6,15 +7,20 @@ def play_game(words, lives):
     word = random.choice(words)
     dashed = len(word) * "-".split()
     letters = set(word)
+    guesses = set()
 
     while lives > 0:
         print()
         print("".join(dashed))
         guess = input("Input a letter: ")
-        if guess in set(dashed):
-            print("No improvements")
-            lives -= 1
+        if guess in guesses:
+            print("You've already guessed this letter")
+        elif len(guess) != 1:
+            print("You should input a single letter")
+        elif guess not in ascii_lowercase:
+            print("Please enter a lowercase English letter")
         elif guess in letters:
+            guesses.add(guess)
             indexes = [index for index, letter in enumerate(word) if letter == guess]
             for i in indexes:
                 dashed[i] = guess
@@ -23,6 +29,7 @@ def play_game(words, lives):
                 print("You survived!")
                 break
         else:
+            guesses.add(guess)
             print("That letter doesn't appear in the word")
             lives -= 1
     else:
